be7db11b5aae20ba026cf0c46199bf8359703b3f
Giovani Clarindo <101987259+giovaniclarindo@users.noreply.github.com>
2022-05-11 12:41:43 -0300

    Add files via upload

---
+++ lista1_exercicio2.php
@@ -0,0 +1,7 @@
+<?php
+	$salBruto = 5000;
+	$desconto = $salBruto * 0.08;
+	$salLiquido = $salBruto - $desconto;
+
+	//exibindo respostas
+	echo "o salario bruto é $salBruto e salario liquido é $salLiquido";
